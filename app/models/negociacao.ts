77f03e148720e88055d8d28814383172a04ec832
Rafaela <rafaelasoperes@gmail.com>
2023-02-10 20:39:07 -0300

    Feat: adicionando mais arquivos Typescript

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,15 +1,15 @@
 export class Negociacao {
-    private _data;
-    private _quantidade;
-    private _valor;
+    private _data: Date;
+    private _quantidade: number;
+    private _valor: number;
 
-    constructor(data, quantidade, valor) {
+    constructor(data: Date, quantidade: number, valor: number) {
         this._data= data;
         this._quantidade = quantidade;
         this._valor= valor;
 
     }
-_
+
     get data (){
         return this._data;
     }
